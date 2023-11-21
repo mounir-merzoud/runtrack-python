de46d7c94601ca5a978eb306e61c1f8469fb6aaa
mounir-merzoud <mounir.merzoud@laplateforme.io>
2023-11-21 15:59:52 +0100

    runtrack-python jour2

---
+++ jour02/job04/main.py
@@ -0,0 +1,12 @@
+#table de multiplication pour i=1
+i = 1
+j = 2
+for n in range(1, 11):
+    produit = i * n
+    print(i, "X", n, "=", produit)
+##table de multiplication pour i=2
+
+i = 2
+for n in range(1, 11):
+    produit = i * n
+    print(i, "X", n, "=", produit)
---
+++ jour02/job06/main.py
@@ -0,0 +1,8 @@
+for i in range(2, 1001):
+    for n in range(2, 1001):
+        if (i == n or i == 2) and n % 2 != 0:
+            print(i)
+            break
+
+
+
